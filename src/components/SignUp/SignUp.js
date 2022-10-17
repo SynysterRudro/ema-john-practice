f6ecfc72c1c00a1ca4682779ad2d81fc28fcc880
SynysterRudro <rudrosingh82@gmail.com>
2022-10-17 17:05:55 +0600

    signup completed

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 import './SignUp.css';
 
 const SignUp = () => {
 
     const [error, setError] = useState(null);
+    const { createUser } = useContext(AuthContext);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -21,6 +23,14 @@ const SignUp = () => {
             setError('Your password did not match');
             return;
         }
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+            })
+            .catch(error => console.error(error))
     }
     return (
         <div className='form-container'>
